c1755137a3cc95afeb9294a8bc3a55e8abe51bfe
bkoenning <brandon.koenning@gmail.com>
2015-10-27 09:51:11 -0500

    Initial Commit

---
+++ Vancomycin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vancomycin
+//
+//  Created by Brandon Koenning on 10/27/15.
+//  Copyright © 2015 Brandon Koenning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VancomycinTests/VancomycinTests.m
@@ -0,0 +1,39 @@
+//
+//  VancomycinTests.m
+//  VancomycinTests
+//
+//  Created by Brandon Koenning on 10/27/15.
+//  Copyright © 2015 Brandon Koenning. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VancomycinTests : XCTestCase
+
+@end
+
+@implementation VancomycinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
